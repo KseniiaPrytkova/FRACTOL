0646846630d35712c78b20b85c7aa7f0b2b8e02f
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-05-27 20:48:58 +0300

    starting the project

--- fractol.c
+++ fractol.c
@@ -9,3 +9,18 @@
 /*   Updated: 2018/05/27 16:37:41 by kprytkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "./includes/fractol.h"
+
+int		main(int argc, char const *argv[])
+{
+	t_env 	*e;
+	if (!(e = malloc(sizeof(t_env))))
+		return (0);
+	e->mlx = mlx_init();
+	e->win = mlx_new_window(e->mlx, WIDTH, HEIGHT, "hello, fractolˆˆ");
+
+	mlx_pixel_put(mlx_ptr, win_ptr, 50, 40, 8388352); 
+	mlx_loop(e->mlx);
+	return (0);
+}
--- includes/fractol.h
+++ includes/fractol.h
@@ -9,3 +9,23 @@
 /*   Updated: 2018/05/27 17:04:40 by kprytkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <mlx.h>
+# include <math.h>
+# include "../includes/libft.h"
+
+# include <stdio.h> /* for printf */
+
+# define WIDTH 		1200
+# define HEIGHT		800
+
+typedef struct		s_env 
+{
+	void		*mlx;
+	void		*win;
+}					t_env;
+#endif
